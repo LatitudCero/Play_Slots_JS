bfdd2926a6585447ebe6f6e3ce0814edbecabc1d
LatitudCero <139180538+LatitudCero@users.noreply.github.com>
2023-08-20 15:10:36 -0500

    insert bar on spin. determine if match for win

--- project.js
+++ project.js
@@ -87,10 +87,43 @@ const spin = () => {
     return reels;
 };
 
-const reels = spin();
-console.log(reels);
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows
+}
+
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if (i != row.length - 1) {
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
+    }
+};
+
+const getWinnings = (rows, bet, lines) => {
+    
+}
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
+
+
 
 
