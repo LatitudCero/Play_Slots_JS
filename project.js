f21b98cf695756841550453ee3b2f122dee9673b
LatitudCero <139180538+LatitudCero@users.noreply.github.com>
2023-08-20 14:08:13 -0500

    adding reels (rows and columns), assigning values, set up reels for spin.

--- project.js
+++ project.js
@@ -8,6 +8,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+}
+
+const SYMBOL_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2,
+}
+
 const deposit = () => {
     while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
@@ -34,21 +51,29 @@ const getNumberOfLines = () => {
     };
 };
 
-const getBet = (balance) => {
+const getBet = (balance, lines) => {
     while (true) {
-        const bet = prompt("Enter the total bet: ");
+        const bet = prompt("Enter the bet per line: ");
         const numberBet = parseFloat(bet);
     
-            if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance) {
+            if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
                 console.log("Invalid bet, try again.")
             } else {
                 return numberBet;
             }
     };
+};
 
-}
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+       console.log(symbol, count);
+    }
+    console.log(symbols);
+};
+spin();
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
-const bet = getBet(balance);
+const bet = getBet(balance, numberOfLines);
 
 
