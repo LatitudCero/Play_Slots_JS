17b7a85eedb0b565aaa9b92ee827370370d244e4
LatitudCero <139180538+LatitudCero@users.noreply.github.com>
2023-08-20 14:45:20 -0500

    running reels to generate random matches

--- project.js
+++ project.js
@@ -74,13 +74,20 @@ const spin = () => {
 
     const reels = [[], [], []];
     for (let i = 0; i < COLS; i++) {
-        
-        for (let j = 0; j < ROWS; j++) {
-
-        }
+        const reelSymbols = [...symbols];
+      for (let j = 0; j < ROWS; j++) {
+        const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        reelSymbols.splice(randomIndex, 1);
+      }
     }
+
+    return reels;
 };
-spin();
+
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
